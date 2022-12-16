570ef5c103bd15eb5ed425ced5a13f3324699851
nimna29 <niwarthananimna@gmail.com>
2022-12-17 01:21:55 +0530

    Import data set

--- preprocess/data_preprocessing.py
+++ preprocess/data_preprocessing.py
@@ -2,3 +2,9 @@
 ## Developed by Nimna Niwarthana ##
 #### Data Pre-processing ####
 
+import numpy as np
+import pandas as pd
+
+# Load dataset
+dataset = pd.read_csv('../samples.csv')
+print(dataset.describe())
