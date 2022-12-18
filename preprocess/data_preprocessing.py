14025d5e4dd4eae61b967e4b088a1771297c2c85
nimna29 <niwarthananimna@gmail.com>
2022-12-18 07:34:06 +0530

    Processed to until feature scaling

--- preprocess/data_preprocessing.py
+++ preprocess/data_preprocessing.py
@@ -10,8 +10,34 @@ dataset = pd.read_csv('../dataset1.csv')
 print(dataset.describe())
 
 
-# create dependent & independent variable vectors
+# Create dependent & independent variable vectors
+x = dataset.iloc[:,4:-4].values #independent
+y = dataset.iloc[:,-3].values #dependent
 
+
+# Handle missing data
+# Count the num of missing values in each column - output = 0
+# print(dataset.isnull().sum()) 
+
+
+# Data Encoding
+# Label Encoding
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+
+# Split the dataset for training & testing
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.30,random_state=42)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
 
 
 
